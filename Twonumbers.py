26c64f2cd2bdd4172e9b16b6bff1891e8c804f64
Tarun dev <devtarunmagadi@gmail.com>
2025-11-21 11:42:07 +0530

    Update Twonumbers.py

--- Twonumbers.py
+++ Twonumbers.py
@@ -1,13 +1,12 @@
 import sys
 if len(sys.argv) != 3:
     print("Usage: python Twonumber.py <num1> <num2>")
-    else:
+else:
     num1=20
     num2=30
-    sys.exit()
     num1 = float(sys.argv[1])
     num2 = float(sys.argv[2])
-
+    sys.exit()
 if num1 > num2:
     print(num1, "is greater")
 elif num2 > num1:
