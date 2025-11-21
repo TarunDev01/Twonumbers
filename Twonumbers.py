bf7e1c9f77adc865f452020ea57a233d86f963b5
Tarun dev <devtarunmagadi@gmail.com>
2025-11-21 11:53:38 +0530

    Update Twonumbers.py

--- Twonumbers.py
+++ Twonumbers.py
@@ -4,11 +4,9 @@ default_num2 = 20
 if len(sys.argv) == 3:
     num1 = float(sys.argv[1])
     num2 = float(sys.argv[2])
-    print("Using command-line values...")
 else:
     num1 = default_num1
     num2 = default_num2
-    print("Using default values...")
 if num1 > num2:
     print(num1, "is greater")
 elif num2 > num1:
